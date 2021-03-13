9c1b17c0af2eb8d5125941bcac0ec100a4cccaeb
Eriveton Costa <erivelton.silva106@gmail.com>
2021-03-13 08:19:25 -0300

    commit initial

---
+++ BookListMVC/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookListMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
